fc226cfeb4ffbec69e65d56c2a89f36f7a678ce3
brian d foy <brian.d.foy@gmail.com>
2008-04-03 09:34:29 +0000

    * Added two new modules that I'm working on for the QA Hackathon
    Module::Extract::Namespaces
    
    Module::Extract::VERSION
    
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@2509 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

---
+++ lib/VERSION.pm
@@ -0,0 +1,135 @@
+# $Id$
+package Module::Extract::VERSION;
+use strict;
+
+use warnings;
+no warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+use Carp qw(carp);
+
+$VERSION = '0.10_01';
+
+=head1 NAME
+
+Module::Extract::VERSION - Extract a module version without running code
+
+=head1 SYNOPSIS
+
+	use Module::Extract::VERSION;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head2 Class methods
+
+=over 4
+
+=item $class->parse_version_safely( FILE );
+
+Given a module file, return the module version. This works just like
+C<mldistwatch> in PAUSE. It looks for the single line that has the
+C<$VERSION> statement, extracts it, evals it, and returns the result.
+
+=cut
+
+sub parse_version_safely # stolen from PAUSE's mldistwatch, but refactored
+	{
+	my $class = shift;
+	my $file = shift;
+	
+	local $/ = "\n";
+	local $_; # don't mess with the $_ in the map calling this
+	
+	my $fh;
+	unless( open $fh, "<", $file )
+		{
+		carp( "Could not open file [$file]: $!\n" );
+		return;
+		}
+	
+	my $in_pod = 0;
+	my $version;
+	while( <$fh> ) 
+		{
+		chomp;
+		$in_pod = /^=(?!cut)/ ? 1 : /^=cut/ ? 0 : $in_pod;
+		next if $in_pod || /^\s*#/;
+
+		next unless /([\$*])(([\w\:\']*)\bVERSION)\b.*\=/;
+		my( $sigil, $var ) = ( $1, $2 );
+		
+		$version = $class->_eval_version( $_, $sigil, $var );
+
+		last;
+		}
+	close $fh;
+
+	return $version;
+	}
+
+sub _eval_version
+	{
+	my $class = shift;
+	
+	my( $line, $sigil, $var ) = @_;
+	
+	#print STDERR "Called with @_\n";
+	
+	my $eval = qq{ 
+		package ExtUtils::MakeMaker::_version;
+
+		local $sigil$var;
+		\$$var=undef; do {
+			$line
+			}; \$$var
+		};
+		
+	my $version = do {
+		local $^W = 0;
+		no strict;
+		eval( $eval );
+		};
+
+	#print STDERR "Version is $version\n";
+
+	return $version;
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is part of a SourceForge project which always has the
+latest sources in CVS, as well as all of the previous releases.
+
+	http://sourceforge.net/projects/brian-d-foy/
+
+If, for some reason, I disappear from the world, one of the other
+members of the project can shepherd this module appropriately.
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+I stole the some of this code from C<mldistwatch> in the PAUSE
+code by Andreas KE<ouml>nig, but I've moved most of it around.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/corpus.t
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+use File::Spec;
+
+use Test::More 'no_plan';
+
+use_ok( 'Module::Extract::VERSION' );
+can_ok( 'Module::Extract::VERSION', qw(parse_version_safely) );
+
+my %Corpus = (
+	'Easy.pm'       => 3.01,   
+	'RCS.pm'        => 1.23,   
+	'Underscore.pm' => "0.10_01",
+	);
+	
+foreach my $file ( sort keys %Corpus )
+	{
+	my $path = File::Spec->catfile( 'corpus', $file );
+	ok( -e $path, "Corpus file [ $path ] exists" );
+	
+	my $version = 
+		eval{ Module::Extract::VERSION->parse_version_safely( $path ) };
+		
+	is( $version, $Corpus{$file}, "Works for $file" );
+	
+	}
